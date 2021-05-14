2ab4999a935454b9141da6bc953540c5517b47dd
Andrew D'Agostino <15709145+andrew-dagostino@users.noreply.github.com>
2021-05-13 21:11:42 -0400

    Update schema, remove unneeded dependency

---
+++ server/schema.sql
@@ -0,0 +1,12 @@
+-- Database: organizr
+
+-- DROP DATABASE organizr;
+
+CREATE DATABASE organizr
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Canada.1252'
+    LC_CTYPE = 'English_Canada.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
