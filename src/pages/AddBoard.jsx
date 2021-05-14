275db80463a3cf1b9d3c2bc595027f4995356f26
Andrew D'Agostino <15709145+andrew-dagostino@users.noreply.github.com>
2021-05-14 17:27:52 -0400

    Add board card

--- src/pages/AddBoard.jsx
+++ src/pages/AddBoard.jsx
@@ -1,5 +1,52 @@
 import React from 'react';
+import { Card, Form, Grid } from 'semantic-ui-react';
 
-export default function AddBoard() {
-    return <>AddBoard</>;
+export default class AddBoard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <Grid doubling stackable container>
+                <Grid.Row centered>
+                    <Grid.Column width="6">
+                        <Card fluid>
+                            <Card.Content>
+                                <Card.Header style={{ marginBottom: '1rem' }}>
+                                    Add Board
+                                </Card.Header>
+                                <Form>
+                                    <Form.Group widths="equal">
+                                        <Form.Input
+                                            label="Board Name"
+                                            type="text"
+                                            required
+                                            fluid
+                                        />
+                                    </Form.Group>
+                                    <Form.Group widths="equal">
+                                        <Form.Input
+                                            label="Board Image (Optional)"
+                                            type="file"
+                                            fluid
+                                        />
+                                    </Form.Group>
+                                    <Form.Button
+                                        type="submit"
+                                        size="medium"
+                                        primary
+                                    >
+                                        Create
+                                    </Form.Button>
+                                </Form>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
+    }
 }
