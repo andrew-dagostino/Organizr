3a8d765a7b2d906f8446e530231427a00de76580
Andrew D'Agostino <andrewda98@live.ca>
2021-05-18 23:07:08 -0400

    Adjsut header centering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -66,7 +66,7 @@ export default class Header extends Component {
                             placeholder="Board Name..."
                             style={{
                                 position: 'absolute',
-                                left: '40%',
+                                left: '44%',
                                 height: '3rem',
                             }}
                         >
